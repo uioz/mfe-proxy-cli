03145ec00583796fe0dce1ef2759f5a6ca08c604
ASCll <hzxbpld@163.com>
2021-04-21 17:43:11 +0800

    fix: #5

--- src/common.js
+++ src/common.js
@@ -2,4 +2,6 @@ module.exports = {
   MANIFEST_FILE_NAME: 'mfe-manifest.json',
   CONFIG_FILE_NAME: 'mfe-config.js',
   ROUTE_FILE_NAME: 'mfe-route.js',
+  DEFAULT_OUTPUT_DIR: 'dist',
+  DEFAULT_STATIC_DIR: 'dist/static',
 };
