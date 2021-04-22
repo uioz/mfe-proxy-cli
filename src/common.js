e50cdfab8a63c97bd3069a87245a50810543cd57
ASCll <hzxbpld@163.com>
2021-04-22 13:57:22 +0800

    fix: 内置的文件名称错误

--- src/common.js
+++ src/common.js
@@ -1,7 +1,7 @@
 module.exports = {
   MANIFEST_FILE_NAME: 'mfe-manifest.json',
   CONFIG_FILE_NAME: 'mfe-config.js',
-  ROUTE_FILE_NAME: 'mfe-route.js',
+  ROUTE_FILE_NAME: 'mfe-route.json',
   DEFAULT_OUTPUT_DIR: 'dist',
   DEFAULT_STATIC_DIR: 'dist/static',
 };
