6a2c5046d9d026dd4c400b89ef1ec367ac9805a6
ASCll <hzxbpld@163.com>
2021-04-25 14:52:42 +0800

    feat: create command basic support

--- bin/index.js
+++ bin/index.js
@@ -10,24 +10,40 @@ program
   )
   .usage('<command> [options]');
 
+// program
+//   .command(`add <packageName...>`)
+//   .description('to adds more package on this project')
+//   .option(
+//     '-r, --registry <url>',
+//     'Use specified npm registry when installing packages'
+//   )
+//   .option(
+//     '-m, --manifest-name <name>',
+//     `Use more specific name instead of '${MANIFEST_FILE_NAME}' by default`,
+//     MANIFEST_FILE_NAME
+//   )
+//   .option(
+//     '--no-manifest',
+//     `Don't genterator ${MANIFEST_FILE_NAME} for server when finish`,
+//     false
+//   )
+//   .action(require('../src/create-command'));
+
 program
-  .command(`create <packageName...>`)
+  .command('create <projectName> <packageName...>')
   .description(
-    `
-    create project with following package name.
-    eg. <@scope/>name<@tag> or <@scope/>name<@tag>=<registry>
-  `
+    `packageName support all that npm install command support.
+packageName also support [packageName]<registry> to specify registry of each package
+eg. [@vue/cli]<http://localhost:8080>`,
+    {
+      projectName: 'a folder',
+      packageName: 'package name that npm install will use it.',
+    }
   )
   .option(
-    '-r, --registry <url>',
-    'Use specified npm registry when installing packages'
+    '-n, --to-npm <anyoption...>',
+    'anything that npm install command support'
   )
-  // .option(
-  //   '-s, --serve',
-  //   'Use mfe-proxy-server host this project at 0,0,0,0:80 when after installed',
-  //   false
-  // )
-  // .option("-p,  --port <port>")
   .option(
     '-m, --manifest-name <name>',
     `Use more specific name instead of '${MANIFEST_FILE_NAME}' by default`,
@@ -40,13 +56,4 @@ program
   )
   .action(require('../src/create-command'));
 
-// process.command('add <packageName...>')
-
-// program
-//   .command("serve")
-//   .description("host")
-//   .action(() => {
-//     // TODO:
-//   });
-
-program.parse(process.argv);
+program.program.parse(process.argv);
